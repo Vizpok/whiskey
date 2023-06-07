b7b9a0598e794d6f115416a35008908e2676aa27
Zenotyi <richiero123@gmail.com>
2023-06-07 12:39:01 -0500

    Diseño

--- Sesion/start_sesion.php
+++ Sesion/start_sesion.php
@@ -23,70 +23,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <html>
 <head>
   <style>
-    body {
-      background-color: #60249B;
-      font-family: Arial, sans-serif;
-    }
-
-    .categoriesDiv {
-      width: 50%;
-      margin: 0 auto;
-    }
-
-    input[type=text],
-    input[type=password],
-    select {
-      width: 100%;
-      padding: 12px 20px;
-      margin: 8px 0;
-      display: inline-block;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      box-sizing: border-box;
-    }
-
-    div {
-      border-radius: 10px;
-      background-color: #BAFF3B;
-      padding: 20px;
-      margin-top: 50px;
-    }
-
-    .button {
-      width: 100%;
-      background-color: #ebc847  ; /* Green */
-      border: none;
-      color: white;
-      padding: 10px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-      margin: 4px 2px;
-      cursor: pointer;
-      border-radius: 12px;
-    }
-
-    h3 {
-      font-weight: bold;
-      color: rgb(53, 0, 132);
-      text-align: center;
-    }
-
-    a {
-      color: rgb(133, 0, 145);
-      text-decoration: none;
-    }
-
-    a:hover {
-      color: rgb(166, 0, 58);
-    }
+    <?php
+      include('styles.css');
+    ?>
   </style>
 </head>
 
 <body>
   <div class="categoriesDiv">
-    <h3>Iniciar Sesión vtalv github2</h3>
+    <h3>Iniciar Sesión</h3>
     <?php if (!empty($error_message)): ?>
       <p style="color: red;"><?php echo $error_message; ?></p>
     <?php endif; ?>
--- Sesion/whconf/act_password.php
+++ Sesion/whconf/act_password.php
@@ -5,63 +5,9 @@
 <html>
   <head>
     <style>
-      body {
-        background-color: #f2f2f2;
-        font-family: Arial, sans-serif;
-      }
-
-      .categoriesDiv {
-        width: 50%;
-        margin: 0 auto;
-      }
-
-      input[type=text],
-      input[type=password],
-      select {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        box-sizing: border-box;
-      }
-
-      div {
-        border-radius: 10px;
-        background-color: #d3fffa;
-        padding: 20px;
-        margin-top: 50px;
-      }
-
-      .button {
-        background-color: #4caf50; /* Green */
-        border: none;
-        color: white;
-        padding: 10px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 4px 2px;
-        cursor: pointer;
-        border-radius: 12px;
-      }
-
-      h3 {
-        font-weight: bold;
-        color: rgb(53, 0, 132);
-        text-align: center;
-      }
-
-      a {
-        color: rgb(133, 0, 145);
-        text-decoration: none;
-      }
-
-      a:hover {
-        color: rgb(166, 0, 58);
-      }
+      <?php
+      include('styles.css');
+      ?>
     </style>
     <h3>Cambiar Contraseña</h3>
   </head>
